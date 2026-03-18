e59cd0826a640d719904d375a42393b648acf4fb
Tarun Sitaraman <mfsbyo@gmail.com>
2026-03-18 13:23:01 +0530

    Update database.js

--- mock-backend/database.js
+++ mock-backend/database.js
@@ -17,6 +17,7 @@ if (usePostgres) {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+    family: 4,
   });
 }
 
